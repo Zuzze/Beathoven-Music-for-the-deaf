f9744d89da53231377d20b9811d1cd5af8a318f2
Nevalainen Susanna <nevalas5@niksula.hut.fi>
2017-04-15 22:13:07 +0300

    neutral words removed in lyrics analyzer increasing the impact of positive/negative words

--- LyricsFeelings/lyricsFeelingCalculator.py
+++ LyricsFeelings/lyricsFeelingCalculator.py
@@ -6,6 +6,33 @@ with open ("lyrics3.txt", "r") as lyricsfile:
     #print("LYRICS: ")
     #print(lyricsData)
 
+    #change lyricsfile manually
+with open ("lyrics3.txt", "r") as lyricsfile:
+    lyricsData=lyricsfile.read().replace('\n', ' ')
+    lyricsData=lyricsData.replace('"', '')
+    lyricsData=lyricsData.split(" ")
+    #print("LYRICS: ")
+    #print(lyricsData)
+
+    #neutral factor
+    neutralWordCount = 0
+    with open ("neutral.txt", "r") as neutralWordsFile:
+        neutralData=neutralWordsFile.read()
+        neutralData=neutralData.split("\n")
+        neutralData = list(filter(None, neutralData))#remove empty strings
+        neutralWords = []
+        for lyricsWord in lyricsData:
+            for neutralWord in neutralData:
+                if lyricsWord == neutralWord:
+                    neutralWordCount+=1
+                    neutralWords.append(neutralWord)
+        print("=== Neutral [0-1]===:" )
+        print(neutralWordCount/(len(lyricsData) - 1.0))
+        print("neutral words: " + str(neutralWordCount))
+        print(neutralWords)
+        print("all words: " + str(len(lyricsData)))
+
+
     #positive words factor
     with open ("positive.txt", "r") as positiveWordsFile:
         posData=positiveWordsFile.read()
@@ -19,10 +46,10 @@ with open ("lyrics3.txt", "r") as lyricsfile:
                     posWordCount+=1
                     posWords.append(posWord)
         print("=== Positive [0-1]===:" )
-        print(posWordCount/(len(lyricsData) - 1.0))
+        print(posWordCount/(len(lyricsData) - 1.0 - neutralWordCount))
         print("positive words: " + str(posWordCount))
         print(posWords)
-        print("all words: " + str(len(lyricsData)))
+        print("all words: " + str(len(lyricsData)-neutralWordCount))
 
     #negative words factor
     with open ("negative.txt", "r") as negativeWordsFile:
@@ -37,7 +64,8 @@ with open ("lyrics3.txt", "r") as lyricsfile:
                     negWordCount+=1
                     negWords.append(negWord)
         print("=== Negative [0-1]===:" )
-        print(negWordCount/(len(lyricsData) - 1.0))
+        print(negWordCount/(len(lyricsData) - 1.0 - neutralWordCount))
         print("negative words: " + str(negWordCount))
         print(negWords)
         print("all words: " + str(len(lyricsData)))
+   
